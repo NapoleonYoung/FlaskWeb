9f171d96b15ba64a6b97f5246bf19b421fa4329f
NapoleonYoung <NapoleonYoung@Pro.local>
2016-05-09 22:14:41 +0800

    把角色写入数据库(v1.06)

--- MyFirstWebServer/app/models.py
+++ MyFirstWebServer/app/models.py
@@ -3,21 +3,52 @@
 
 __author__ = 'NapoleonYoung'
 
-from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import login_manager
-from flask.ext.login import UserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
+from flask.ext.login import UserMixin
+from . import db, login_manager
+
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)    #unique表示这列不允许出现重复的值
-    users = db.relationship('User', backref='role')
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': (Permission.FOLLOW |
+                     Permission.COMMENT |
+                     Permission.WRITE_ARTICLES, True),
+            'Moderator': (Permission.FOLLOW |
+                          Permission.COMMENT |
+                          Permission.WRITE_ARTICLES |
+                          Permission.MODERATE_COMMENTS, False),
+            'Administrator': (0xff, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
 
     def __repr__(self):
-        return '<Role %r>' %self.name
+        return '<Role %r>' % self.name
+
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -25,10 +56,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))  #db.ForeignKey()的参数'roles.id'表明这列的值是roles表中行id的值
-
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
-
 
     @property
     def password(self):
@@ -41,8 +70,8 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    def generate_confirmation_token(self, expirtation=3600):
-        s = Serializer(current_app.config['SECRET_KEY'], expirtation)
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'confirm': self.id})
 
     def confirm(self, token):
@@ -55,12 +84,11 @@ class User(UserMixin, db.Model):
             return False
         self.confirmed = True
         db.session.add(self)
-        db.session.commit()
         return True
 
     def generate_reset_token(self, expiration=3600):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
-        return s.dumps({'resetkey': self.id})
+        return s.dumps({'reset': self.id})
 
     def reset_password(self, token, new_password):
         s = Serializer(current_app.config['SECRET_KEY'])
@@ -68,11 +96,10 @@ class User(UserMixin, db.Model):
             data = s.loads(token)
         except:
             return False
-        if data.get('resetkey') != self.id:
+        if data.get('reset') != self.id:
             return False
         self.password = new_password
         db.session.add(self)
-        db.session.commit()
         return True
 
     def generate_email_change_token(self, new_email, expiration=3600):
@@ -85,7 +112,7 @@ class User(UserMixin, db.Model):
             data = s.loads(token)
         except:
             return False
-        if data.get('change_email') !=self.id:
+        if data.get('change_email') != self.id:
             return False
         new_email = data.get('new_email')
         if new_email is None:
@@ -94,14 +121,12 @@ class User(UserMixin, db.Model):
             return False
         self.email = new_email
         db.session.add(self)
-        db.session.commit()
         return True
 
     def __repr__(self):
-        return '<User %r>' %self.name
+        return '<User %r>' % self.username
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-
